0175d5410152e82c13ecb495b4e63ecfc2f8e8dd
kaelzhang81 <zhangy1981@qq.com>
2015-04-28 23:14:54 +0800

    add tescase

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '5a06c0544bf1cb25224cc2c6bc9715589aef0af5ab3e63b9c6567713e1ff75a87a97ec0b391938f91015161ea4f886c0f8c8de1c2629ed3804554564ad0ad71f'
