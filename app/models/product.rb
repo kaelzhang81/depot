ba563e0ee5406fb3c1bb39928d7337a5e8257bdb
kaelzhang81 <zhangy1981@qq.com>
2015-04-29 22:48:51 +0800

    add 1st catalog

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ActiveRecord::Base
   validates :description, :presence => true
   validates :image_url, :format => {:with => %r{\.(gif|jpg|png)$}i, :message => 'must be a URL for GIF, JPG or PNG image.', multiline: true}
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  default_scope :order => 'title'
 end
